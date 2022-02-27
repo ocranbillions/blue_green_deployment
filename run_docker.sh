8dc1d60c019d32c3a332f64d47cb4898376b030c
Samuel Ocran <sammiestt@gmail.com>
2022-02-27 19:36:53 +0100

    Cleanup

--- run_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker build --tag=ocranbillions/capstone .
-
-docker image ls
-
-docker run -p 8080:8080 ocranbillions/capstone
--- run_kubernetes.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-dockerpath="ocranbillions/capstone"
-
-kubectl run --image=ocranbillions/capstone capstone --port=8080
-
-kubectl get pod
-
-kubectl port-forward pod/capstone 8080:8080
